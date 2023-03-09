dbd60fa1ed5fb0ad0bae72b9ba506b198317bd82
Karan Monga <38899841+karan-monga@users.noreply.github.com>
2023-03-09 15:24:27 -0500

    initial

---
+++ client/src/Components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../assets/css/hero.css';
+
+export const Hero = () => {
+  return (
+    <div className='hero-wrapper'>
+        <div className='heading-wrapper'>
+            <h1><strong>Fast</strong> & easy-to-use <br/> image compression</h1>
+        </div>
+        
+    </div>
+    
+  )
+}
+
+export default Hero
